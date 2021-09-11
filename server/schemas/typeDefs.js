37f2aa0ded5f30602da84e4abe10bacddd30e717
josephn90 <j.ndububa@gmail.com>
2021-09-11 13:35:12 -0400

    login and adduser mutations added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,14 @@ const typeDefs = gql`
 type Query {
     me: User
   }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
